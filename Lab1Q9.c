64e6c13dbfab0dbdc5fc77b8b40f559b2f6cb8b3
ReubenMifsud13 <reuben.mifsud.24@um.edu.mt>
2024-10-15 12:24:53 +0200

    Add files via upload

---
+++ Lab1Q9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+    int A3stock = 1000, A4stock = 1000, A5stock = 1000; // preset stock
+    int newA3, newA4, newA5; //variables to store user input
+    //Prompting & reading user input:
+    printf("Please insert amount of A3 packs");
+    scanf("%d", &newA3);
+    A3stock += newA3;
+
+    printf("Please insert amount of A4 packs");
+    scanf("%d", &newA4);
+    A4stock += newA4;
+
+    printf("Please insert amount of A5 packs");
+    scanf("%d", &newA5);
+    A5stock += newA5;
+    
+    printf("Updated stock:\nA3 packs:\t %d\nA4 packs:\t %d\nA5 packs:\t %d", A3stock,A4stock,A5stock);
+    return 0;
+}
